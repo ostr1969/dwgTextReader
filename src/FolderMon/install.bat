e62fff538e051d002a08b6cc8d9ac17d9949690c
barako <ostr1969@gmail.com>
2025-09-30 12:00:33 +0300

    light

--- src/FolderMon/install.bat
+++
@@ -1,8 +0,0 @@
-﻿set DWDCRAWLER_FOLDER=C:\Users\barako\source\repos\ACadSharp\samples
-cd C:\Users\barako\source\repos\ACadSharp\src\FolderMon\bin\Debug\net8.0
-sc delete FolderMonitorService 
-sc create FolderMonitorService binPath= C:\Users\barako\source\repos\ACadSharp\src\FolderMon\bin\Debug\net8.0\FolderMon.exe
-sc start FolderMonitorService 
-
-rem sc stop FolderMonitorService 
-
--- src/vscodeBlazorApp/Components/Models/Presenter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace vscodeBlazorApp.Components.Models
-{
-    public class Presenter
-    {
-        public string file { get; set; }
-        public string HtmlContent { get; set; }
-        public string plain { get; set; }
-    }
-}
